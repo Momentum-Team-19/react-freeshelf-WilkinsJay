3e2b5197fdc885725b5f0750305260e61c2ac415
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-09-07 15:00:58 -0400

    start of freeshelf with some errors

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css'
 import bookData from './book-data.json'
 
 function App() {
-  return (
-    <div>
-      <h1>Freeshelf</h1>
-      {/* Use bookData to show books on the page*/}
-    </div>
-  )
+    return (
+        <div>
+            <h1>Freeshelf</h1>
+            {/* Use bookData to show books on the page*/}
+        </div>
+    )
 }
+
+// const BookTitle = ({ title }) => {
+//     return (
+//         <h1>{title}</h1>
+//     );
+// }
+
+// export default BookTitle;
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import BookTitle from './BookTitle';
+import BookAuthor from './BookAuthor';
+import BookDescription from './BookDescription';
+import BookCover from './BookCover';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
+const BookPage = () => {
+    const book = {
+        title: 'The Secret',
+        author: 'Napolean Hill',
+        description: '',
+        coverUrl: '',
+    };
+
+    return (
+        <div>
+            <BookTitle title={book.title} />
+            <BookAuthor author={book.author} />
+            <BookDescription description={book.description} />
+            <BookCover coverUrl={book.coverUrl} />
+        </div>
+    );
+}
+
+export default BookPage;
