39f34de413ae2dbfde53e1ba43ab0d4641846b20
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-09-08 00:51:15 -0400

    Every book is on the page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
+import { useState } from 'react'
 import './App.css'
 import bookData from './book-data.json'
 
 function App() {
+
+    const [showMore, setShowMore] = useState(false)
     return (
-        <div>
-            <h1>Freeshelf</h1>
+        <div className='App'>
             {/* Use bookData to show books on the page*/}
+
+            {
+                bookData.map((book) => (
+                    <div className='book'>
+                        <Book title={book.title}
+                            author={book.author}
+                            coverImageUrl={book.coverImageUrl}
+                            shortDescription={book.shortDescription}
+                        />
+                        <button onClick={() => {
+                            setShowMore(true)
+                        }} >More Information
+                        </button>
+                        {/* showMore==true ?(
+                        {book.detailedDescription}
+                        ):(
+                            null
+                        ) */}
+                    </div>
+                ))
+            }
         </div>
+
     )
 }
 
-// const BookTitle = ({ title }) => {
-//     return (
-//         <h1>{title}</h1>
-//     );
-// }
+function Book({ title, author, coverImageUrl, shortDescription }) {
+    return (
+        <div className='book-card'>
+            <h3 className='book-title'>Title: {title}</h3>
+            <h4 className='book-author'> By: {author}</h4>
+            <p className='book-description'>{shortDescription}</p>
+            <img className='book-cover' src={coverImageUrl}></img>
+        </div>
+    )
 
-// export default BookTitle;
+}
 
 export default App
