71eeffaec58e68a618a2b7b886ff7becc5783d05
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-09-08 15:50:49 -0400

    finishing touches

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import bookData from './book-data.json'
 
 function App() {
 
-    const [showMore, setShowMore] = useState(false)
     return (
         <div className='App'>
             {/* Use bookData to show books on the page*/}
@@ -15,17 +14,12 @@ function App() {
                         <Book title={book.title}
                             author={book.author}
                             coverImageUrl={book.coverImageUrl}
+                            url={book.url}
+                            publisher={book.publisher}
+                            publicationDate={book.publicationDate}
                             shortDescription={book.shortDescription}
+                            detailedDescription={book.detailedDescription}
                         />
-                        <button onClick={() => {
-                            setShowMore(true)
-                        }} >More Information
-                        </button>
-                        {/* showMore==true ?(
-                        {book.detailedDescription}
-                        ):(
-                            null
-                        ) */}
                     </div>
                 ))
             }
@@ -34,13 +28,24 @@ function App() {
     )
 }
 
-function Book({ title, author, coverImageUrl, shortDescription }) {
+function Book({ title, author, coverImageUrl, url, publisher, publicationDate, shortDescription, detailedDescription }) {
+    const [showMore, setShowMore] = useState(false)
+    const toggleDescription = () => {
+        setShowMore(!showMore);
+    }
     return (
         <div className='book-card'>
             <h3 className='book-title'>Title: {title}</h3>
             <h4 className='book-author'> By: {author}</h4>
             <p className='book-description'>{shortDescription}</p>
+            <button onClick={toggleDescription}>
+                {showMore ? 'Show Less' : 'Show More'}
+            </button>
+            {showMore === true ? <p className='detailedDescription'>{detailedDescription}</p> : null}
             <img className='book-cover' src={coverImageUrl}></img>
+            <a className='book-url' href={url}>{url}</a>
+            <h5 className='book-publisher'>{publisher}</h5>
+            <h5 className='book-published'>{publicationDate}</h5>
         </div>
     )
 
